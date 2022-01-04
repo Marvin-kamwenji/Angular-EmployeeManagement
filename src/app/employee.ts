1d24ed3c7d524f25f3a2ad62d1c376b43145c333
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-01-04 11:06:13 +0300

    Second Commit

---
+++ src/app/employee.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+
+   constructor(){
+    this.id = 123;
+    this.firstName = "";
+    this.lastName = "";
+    this.email = "";
+   }
+}
